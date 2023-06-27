b547d54f161f48fa2212e0c64fc3affdd6edffb6
Rick Umali <rickumali@gmail.com>
2023-06-26 22:14:46 -0400

    Tries to print the cookies
    This code for some reason doesn't print anything

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('Cookies: ', req.cookies);
+
   res.render('index', { title: 'Express' });
 });
 
