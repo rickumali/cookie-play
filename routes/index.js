f09a1ca1cd498901b7b59be55c21e64fe06589d8
Rick Umali <rickumali@gmail.com>
2023-06-26 23:00:46 -0400

    Removes console.log()

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('Cookies: ', req.cookies);
-
   res.render('index', { title: 'Express' });
 });
 
